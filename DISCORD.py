db392982edc2d8e9fc441b5ef6866740d60078e7
Fedor <Chernykh.fdor@mail.ru>
2022-05-13 16:08:44 +0300

    Обновление бота 1.1.1
    Добавлена функция начала и остановки спама в какой либо чат, mention всё ещё не работает, с ним какие-то технические шоколадки.
    Цели:
    1. Пофиксить упоминание человека.
    2. Сделать меню по команде help.
    3. Сделать систему поиска разных видео на YouTube + проигрывать их в голосовом канале по команде"".
    4. Сделать систему уровней.
    5. Сделать чтобы бот мог по команде кикать всех с сервера.
    6. Сделать чтобы бот мог банить участников сервера по команде.

--- DISCORD.py
+++ DISCORD.py
@@ -1,5 +1,6 @@
 import discord
 
+from discord.ext import tasks
 from dotenv import load_dotenv, find_dotenv
 from os import getenv
 
@@ -7,6 +8,11 @@ load_dotenv(find_dotenv())
 
 
 class MyClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.background_task.start()
+
     @staticmethod
     async def on_ready():
         print("Bot is ready")
@@ -16,11 +22,44 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
         """Проверяет, что написал человек и отвечает"""
-        if message.content == "Привет":
+        if message.content.lower() == "привет":
             await message.channel.send("Привет")
+        if message.content.lower() == "стоп":
+            await message.channel.send("Кибератака завершена")
+            _Spam.setIsStatusSpam(True)
+        if message.content.lower() == "старт":
+            await message.channel.send("Начинаю кибератаку")
+            _Spam.setIsStatusSpam(False)
+
+    @tasks.loop(seconds=5)
+    async def background_task(self):
+        # await self.get_channel(888702867244978221).send(self.get_user(722516249002901547).mention)
+        if _Spam.getIsStatusSpam() == False:
+            await self.get_channel(862661837043925015).send(self.get_user(722516249002901547).mention(self.user))
+
+
+    @background_task.before_loop
+    async def before_my_task(self):
+        await self.wait_until_ready()
+
+class Spam():
+
+  def __init__(self):
+    self.isStop: bool = False
+
+  def setIsStatusSpam(self, arg: bool = True):
+    self.isStop = arg
+
+  def getIsStatusSpam(self):
+    return self.isStop
+
+
+
 
 
 client = MyClient()
 
 if __name__ == "__main__":
+    _Spam = Spam()
     client.run(getenv("token"))
+
